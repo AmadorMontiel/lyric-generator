10d9a10fcd3fb39c9738976a62f34619d55772fc
Amador Montiel <61609445+AmadorMontiel@users.noreply.github.com>
2021-07-04 23:22:09 -0700

    Created lyric array

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log('Test Script');
+let lyricArray = ['Know this I will return to this land Rebuild where the ruins did stand Chain of the demons set free Strange alchemy',
+'He gave his life for us He fell upon the Cross To die for all of those Who never mourn His loss',
+`So understand Don't waste your time always searching for those wasted years Face up, make your stand Realize you're living in the golden years`,
+`No, no we can't let them take anymore No we can't let them take anymore We've the land of the free`];
+
